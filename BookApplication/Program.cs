831f1f905911cd877be1b9bb52c5da0bd9187caf
trysuraj <devsurajudeen@gmail.com>
2023-05-27 10:43:50 -0700

    registering CORS

--- BookApplication/Program.cs
+++ BookApplication/Program.cs
@@ -16,18 +16,26 @@ builder.Services.AddDbContextPool<BookDbContext>(options =>
 builder.Services.AddTransient<BookDbContext>();
 
 builder.Services.AddScoped<IBookService, BookService>();
+builder.Services.AddCors(options => options.AddPolicy("corspolicy",builder => 
+{
+    builder.WithOrigins( "http://localhost:3000").AllowAnyMethod().AllowAnyHeader();
+}));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors(c =>
+{
+    c.AddPolicy("AllowOrigin", options => options.AllowAnyOrigin());
+});
 
 //builder.Services.AddAutoMapper(typeof(Program).Assembly);
 builder.Services.AddServices();
 builder.Services.AddCustomConfiguredAutoMapper();
 
 var app = builder.Build();
-
+app.UseCors("corspolicy");
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
